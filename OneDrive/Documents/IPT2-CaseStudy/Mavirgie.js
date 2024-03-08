c27a8094e218b10d17225dbc39ecb7b1fff82f8d
Jay Mark Melivo <melivojaymark61@gmail.com>
2024-03-08 22:32:25 +0800

    final

---
+++ OneDrive/Documents/IPT2-CaseStudy/Mavirgie.js
@@ -0,0 +1,10 @@
+function myFunction() {
+document.getElementById("demo").innerHTML = "Thankyou for trying.";
+}
+function MyFunction() {
+document.getElementById("hide").style.display = "none";
+}
+
+function MYFunction() {
+    document.getElementById("hide").style.display = "block";
+}
