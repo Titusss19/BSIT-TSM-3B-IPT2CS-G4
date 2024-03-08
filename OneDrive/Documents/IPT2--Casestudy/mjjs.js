8dd240b0c61f26e15d847131c9bfef29c98776d1
Jay Mark Melivo <melivojaymark61@gmail.com>
2024-03-08 20:45:32 +0800

    adding some javascript

---
+++ OneDrive/Documents/IPT2--Casestudy/mjjs.js
@@ -0,0 +1,8 @@
+document.getElementById("button1").addEventListener("click", function() {
+    alert("You only live once, but if you do it right, once is enough.");
+  });
+
+
+  document.getElementById("button2").addEventListener("click", function() {
+    alert("It is better to be hated for what you are than to be loved for what you are not.");
+  });
